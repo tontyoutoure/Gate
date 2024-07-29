5782456a7a9a41c426967d7294a6edcf5df61bbd
Radia Oudihat <radia.oudihat@yahoo.com>
2024-07-29 14:01:29 +0200

    modification

--- source/digits_hits/include/GateCoincidenceMultiplesKiller.hh
+++ source/digits_hits/include/GateCoincidenceMultiplesKiller.hh
@@ -1,5 +1,9 @@
-
-
+/*----------------------
+   Copyright (C): OpenGATE Collaboration
+This software is distributed under the terms
+of the GNU Lesser General  Public Licence (LGPL)
+See LICENSE.md for further details
+----------------------*/
 #ifndef GateCoincidenceMultiplesKiller_h
 #define GateCoincidenceMultiplesKiller_h 1
 
